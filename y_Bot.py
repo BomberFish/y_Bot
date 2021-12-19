ecb3a4d55d0fc53f23fdbec23385e32bd1e80a92
DALEK-77 <56525810+DALEK-77@users.noreply.github.com>
2021-12-19 14:25:33 -0500

    resistance is futile

--- y_Bot.py
+++ y_Bot.py
@@ -3,40 +3,40 @@ import discord
 from fractions import Fraction
 
 class Command:
-    def __init__(self, command, the_help_for_the_command, the_number_of_arguments_the_command_takes):
+    def __init__(self, command, cmdhelp, argnum):
         self.command = command
-        self.the_help_for_the_command = the_help_for_the_command
-        self.the_number_of_arguments_the_command_takes = the_number_of_arguments_the_command_takes
+        self.cmdhelp = cmdhelp
+        self.argnum = argnum
 
 class Y_Bot_Exception(Exception):
     pass
 
-the_list_of_commands = [Command("ping", "pong", 0), Command("pong", "ping", 0), Command("conv", "calculate a linear equation", 2), Command("help", "gives you help", 0), Command("morse", "convert morse to text and vice-versa", 1)]
+the_list_of_commands = [Command("ping", "pong", 0), Command("pong", "ping", 0), Command("conv", "calculate a linear equation", 2), Command("help", "gives you help", 0), Command("morse", "convert morse to text and vice-versa", 1), Command("echo", "Echoes text to you", 1)]
 # SEP 11 2021 @ 19:09:30
 # the_list_of_commands = [Command("ping", "pong", 0), Command("pong", "ping", 0), Command("conv", "calculate a linear equation", 2), Command("help", "gives you help", 0), Command("pin", "test the automatic abbreviations", 0), Command("morse", "convert morse to text and vice-versa", 1)]
 
 def parse_command(command, allow_abbreviations=True):
-    the_list_that_we_pair_down = the_list_of_commands[:]
+    pairlist = the_list_of_commands[:]
     if allow_abbreviations:
         i = 0
         while i < len(command.command):
             j = 0
-            while j < len(the_list_that_we_pair_down):
-                if (len(the_list_that_we_pair_down[j].command) > i and the_list_that_we_pair_down[j].command[i] != command.command[i]) or len(the_list_that_we_pair_down[j].command) <= i:
-                    del the_list_that_we_pair_down[j]
+            while j < len(pairlist):
+                if (len(pairlist[j].command) > i and pairlist[j].command[i] != command.command[i]) or len(pairlist[j].command) <= i:
+                    del pairlist[j]
                     j -= 1
                 j += 1
             i += 1
             
-        if len(the_list_that_we_pair_down) == 1:
-            return the_list_that_we_pair_down[0]
-        elif len(the_list_that_we_pair_down) == 0:
+        if len(pairlist) == 1:
+            return pairlist[0]
+        elif len(pairlist) == 0:
             raise Y_Bot_Exception(f"Command not found: {command.command}")
         else:
-            for x in the_list_that_we_pair_down:
+            for x in pairlist:
                 if x.command == command.command:
                     return x
-            raise Y_Bot_Exception(f"Ambiguous command \"{command.command}\"", f"possibilities are: {', '.join([x.command for x in the_list_that_we_pair_down])}")
+            raise Y_Bot_Exception(f"Ambiguous command \"{command.command}\"", f"possibilities are: {', '.join([x.command for x in pairlist])}")
 
 async def do_command(command, message, the_rest_of_the_command):
     if command.command == "ping":
@@ -45,7 +45,7 @@ async def do_command(command, message, the_rest_of_the_command):
         await message.channel.send("ping")
     elif command.command == "conv":
         the_rest_of_the_command = the_rest_of_the_command.split()
-        if len(the_rest_of_the_command) < command.the_number_of_arguments_the_command_takes:
+        if len(the_rest_of_the_command) < command.argnum:
             raise Y_Bot_Exception(f"Not enough arguments provided for command: {command.command}")
         the_strings_in_the_rest_of_the_command_that_actually_matter = the_rest_of_the_command[:min(len(the_rest_of_the_command), 3)]
         the_three_ratios = [Fraction(x) for x in the_strings_in_the_rest_of_the_command_that_actually_matter]
@@ -58,8 +58,8 @@ async def do_command(command, message, the_rest_of_the_command):
         the_linear_equation = the_three_ratios[0] * the_three_ratios[1] + the_three_ratios[2]
         await message.channel.send(str(the_linear_equation) if not should_it_be_a_float else str(float(the_linear_equation)))
     elif command.command == "help":
-        the_list_that_we_pair_down = the_list_of_commands[:]
-        await message.channel.send(embed=discord.Embed(title=f"Commands are:", color=0x00ff88, description={', '.join([x.command for x in the_list_that_we_pair_down])}))
+        pairlist = the_list_of_commands[:]
+        await message.channel.send(embed=discord.Embed(title=f"Commands are:", color=0x00ff88, description={', '.join([x.command for x in pairlist])}))
     elif command.command == "":
         await message.channel.send(embed=discord.Embed(title=f"Whoops!", color=0xff0000, description="You didn't pass any commands to me!"))
     # elif command.command == "pin":
@@ -87,6 +87,8 @@ async def do_command(command, message, the_rest_of_the_command):
     #     the_other_thing_to_add = [x.split(" ") for x in the_rest_of_the_command.split("/")]
     #     print(the_rest_of_the_command)
     #     await message.channel.send(the_rest_of_the_command)
+    elif command.command == "be-a-robot":
+        raise Y_Bot_Exception(f"Resistance is Futile.")
 
 class Y_Bot(discord.Client):
     async def on_ready(self):
